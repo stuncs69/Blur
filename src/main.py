bd0f6a174d8a3eaa629da8715da8446b88e1df7a
helix <68465860+HelixDev@users.noreply.github.com>
2022-05-23 15:46:46 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 import threading, sockets
 import threading as thr
 
+mem = []
+
+async def get_setting(setting):
+	with open('./settings.blr.config') as set_file:
+		for x in set_file.read().split('\n'):
+			if x.startswith(f'{setting}='):
+				return x.rstrip(f"{setting}=")
+		return 0
+
