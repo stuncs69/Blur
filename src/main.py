7a985ef2b961cac744e6227a56d46a025bd7a69a
helix <68465860+HelixDev@users.noreply.github.com>
2022-05-24 16:15:48 +0200

    added wargaming.net

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
-import threading, sockets
+import threading, socket
 import threading as thr
 
 mem = []
 
 async def get_setting(setting):
+	setting = setting.upper()
 	with open('./settings.blr.config') as set_file:
 		for x in set_file.read().split('\n'):
 			if x.startswith(f'{setting}='):
-				return x.rstrip(f"{setting}=")
+				return x.lstrip(f"{setting}=")
 		return 0
 
+def main():
+	while True:
+		s = socket.socket().setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADRR).bind(('0.0.0.0',7060)).listen(5)
+		
+
+
+if __name__ == "__main__":
+	main()
