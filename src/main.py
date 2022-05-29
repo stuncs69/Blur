81caf4a4e01f3a886535500854a1f0111f9542b8
helix <68465860+HelixDev@users.noreply.github.com>
2022-05-29 20:46:18 +0200

    added stuff

--- src/main.py
+++ src/main.py
@@ -30,12 +30,14 @@ def runtime(command):
 	y = command.split('--').strip(' ')
 	main_cmd = command.split(' ')[0]
 	valid_commands = ['chvar','advar','dlvar','rnvar']
-	z = []
+	z = [] # assigned for data and options
 	if main_cmd in valid_commands:
 		for x in y:
 			b = x.split('=')
 			x = x.lstrip(f"{b[0]}=").strip('"')
 			z.append({f'{b[0]}':x})
+		if main_cmd.startswith('chvar'):
+			
 		
 	else:
 		return 0
@@ -50,9 +52,10 @@ def main():
 		with y:
 			z = Buffer(y)
 			while True:
-				line = b.get_line()
+				line = z.get_line()
 				if line == None:
 					break
+				thr.Thread(target=runtime,args=(line)).start()
 				runtime(line)
 		print(f'{adrr} DISCONNECT')
 
